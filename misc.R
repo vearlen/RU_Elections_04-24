1350610eb982f1911660fa0c47d8b32c60772a3e
ilya <admin@Ilya.local>
2024-03-15 23:52:57 +0100

    update

--- misc.R
+++ misc.R
@@ -1,5 +1,17 @@
 RU04_20 %>% 
-  filter(year == 2018,en_country == 'Czechia',
+  filter(year == 2018,en_country == 'Latvia',
          Label=='Число действительных избирательных бюллетеней') %>% 
   # View()
   summarise(sum = sum(number))
+
+
+
+RU04_20 %>% 
+  filter(year == 2018,
+         region == 'Europe',
+         Label=='Число действительных избирательных бюллетеней') %>% 
+  group_by(en_country) %>% 
+  summarise(sum = sum(number)) %>% 
+  arrange(-sum) %>% 
+  write.csv('Data/2018_voted.csv',row.names = FALSE)
+  
